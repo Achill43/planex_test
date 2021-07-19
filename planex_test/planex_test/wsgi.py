da5fe3c5c238571074b88f407faffaf7deb51ef7
Sergei Sorochinskii <sergei.ahill@gmail.com>
2021-07-19 20:06:30 +0300

    Truid Fixed heroku error

--- planex_test/planex_test/wsgi.py
+++ planex_test/planex_test/wsgi.py
@@ -8,9 +8,9 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "planex_test.settings")
 
 from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
 
-application = Cling(get_wsgi_application())
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'planex_test.settings')
+
+application = get_wsgi_application()
