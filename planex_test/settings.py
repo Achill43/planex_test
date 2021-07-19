64a0aac88eb1d17a40bfe97698207653aaff7449
Sergei Sorochinskii <sergei.ahill@gmail.com>
2021-07-19 20:57:06 +0300

    Fixed heroku allowed hosts

--- planex_test/settings.py
+++ planex_test/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get(
 DEBUG = True
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["127.0.0.1", "vast-woodland-70932.herokuapp.com"]
 
 
 # Application definition
