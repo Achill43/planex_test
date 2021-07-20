67527ac7707db2dd174427e46da594791ab91fb2
Sergei Sorochinskii <sergei.ahill@gmail.com>
2021-07-20 11:42:58 +0300

    Change celery broker url

--- planex_test/celery.py
+++ planex_test/celery.py
@@ -7,7 +7,9 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'planex_test.settings')
 app = Celery('planex_test')
 app.config_from_object('django.conf:settings')
 
-app.conf.update(BROKER_URL=os.environ.get('REDIS_URL'),
-                CELERY_RESULT_BACKEND=os.environ.get('REDIS_URL'))
+app.conf.update(
+    BROKER_URL='redis://:p8b4cfe5e06377ca95a069555cfb166daaa11d1c037033a0f0fde1fcb8f8301c9@ec2-44-194-38-94.compute-1.amazonaws.com:10540',
+    CELERY_RESULT_BACKEND='redis://:p8b4cfe5e06377ca95a069555cfb166daaa11d1c037033a0f0fde1fcb8f8301c9@ec2-44-194-38-94.compute-1.amazonaws.com:10540'
+)
 
 app.autodiscover_tasks()
