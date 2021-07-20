0bce057ab320277460b01c471a3a49e27bc0297a
Sergei Sorochinskii <sergei.ahill@gmail.com>
2021-07-20 14:07:41 +0300

    Add default REDIS_URL for celery

--- planex_test/celery.py
+++ planex_test/celery.py
@@ -8,8 +8,10 @@ app = Celery('planex_test')
 app.config_from_object('django.conf:settings')
 
 app.conf.update(
-    BROKER_URL=os.environ.get('REDIS_URL'),
-    CELERY_RESULT_BACKEND=os.environ.get('REDIS_URL'),
+    BROKER_URL=os.environ.get(
+        'REDIS_URL', 'redis://:p8b4cfe5e06377ca95a069555cfb166daaa11d1c037033a0f0fde1fcb8f8301c9@ec2-44-194-38-94.compute-1.amazonaws.com:10540'),
+    CELERY_RESULT_BACKEND=os.environ.get(
+        'REDIS_URL', 'redis://:p8b4cfe5e06377ca95a069555cfb166daaa11d1c037033a0f0fde1fcb8f8301c9@ec2-44-194-38-94.compute-1.amazonaws.com:10540'),
     BROKER_POOL_LIMIT=None
 )
 
