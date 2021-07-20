50a787c32f8a0e259a2fbe2dca5b13d50aa65641
Sergei Sorochinskii <sergei.ahill@gmail.com>
2021-07-20 11:37:32 +0300

    Take REDIS_URL from enviroment variable

--- planex_test/celery.py
+++ planex_test/celery.py
@@ -7,7 +7,7 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'planex_test.settings')
 app = Celery('planex_test')
 app.config_from_object('django.conf:settings')
 
-app.conf.update(BROKER_URL=os.environ['REDIS_URL'],
-                CELERY_RESULT_BACKEND=os.environ['REDIS_URL'])
+app.conf.update(BROKER_URL=os.environ.get('REDIS_URL'),
+                CELERY_RESULT_BACKEND=os.environ.get('REDIS_URL'))
 
 app.autodiscover_tasks()
